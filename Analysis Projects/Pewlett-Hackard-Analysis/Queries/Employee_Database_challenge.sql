a5f6b6bda99f3a0102f465cbb9dac70f4d081c27
roderickspells <59187034+roderickspells@users.noreply.github.com>
2021-06-04 13:18:43 -0500

    Added mentorship_csv and completed Deliverable 2

--- Analysis Projects/Pewlett-Hackard-Analysis/Queries/Employee_Database_challenge.sql
+++ Analysis Projects/Pewlett-Hackard-Analysis/Queries/Employee_Database_challenge.sql
@@ -39,4 +39,35 @@ SELECT * FROM retiring_titles
 
 --DELIVERABLE 2
 
+-- Determine mentorship eligibility 
+--Retrieve relevant employee information
+SELECT e.emp_no, e.first_name, e.last_name, e.birth_date
+FROM employees AS e;
+
+SELECT de.from_date, de.to_date
+FROM dept_emp AS de;
+
+SELECT ti.title
+FROM titles AS ti;
+
+SELECT DISTINCT ON (e.emp_no) 
+				e.emp_no,
+				e.first_name, 
+				e.last_name, 
+				e.birth_date, 
+				de.from_date, 
+				de.to_date,
+				ti.title
+-- Join tables using INTO to determine eligibility
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_emp AS de
+		ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS ti
+		ON (de.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '01-01-1965' AND '12-31-1965')
+		AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
+
+
 
